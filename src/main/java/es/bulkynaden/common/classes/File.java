cffd6af9466a2a438ad3d3d55f0103774e47ceb6
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-06-26 13:27:33 +0200

    Cambio implementation lombok

--- src/main/java/es/bulkynaden/common/classes/File.java
+++ src/main/java/es/bulkynaden/common/classes/File.java
@@ -1,2 +1,30 @@
-package es.bulkynaden.common.classes;public class File {
+package es.bulkynaden.common.classes;
+
+import lombok.Data;
+
+/**
+ * The File class represents a file with properties such as name, size, data, and type.
+ * It implements the IFile interface.
+ */
+@Data
+public class File implements IFile {
+    /**
+     * The name of the file.
+     */
+    private String name;
+
+    /**
+     * The size of the file in bytes.
+     */
+    private long size;
+
+    /**
+     * The data of the file represented as a byte array.
+     */
+    private byte[] data;
+
+    /**
+     * The type of the file.
+     */
+    private String type;
 }
--- src/main/java/es/bulkynaden/common/classes/IFile.java
+++ src/main/java/es/bulkynaden/common/classes/IFile.java
@@ -1,2 +1,62 @@
-package es.bulkynaden.common.classes;public interface IFile {
+package es.bulkynaden.common.classes;
+
+/**
+ * The IFile interface represent a file.
+ */
+public interface IFile {
+    /**
+     * Returns the name of the file.
+     *
+     * @return The name of the file as a String.
+     */
+    String getName();
+
+    /**
+     * Assigns the name of the file.
+     *
+     * @param name A String representing the name of the file.
+     */
+    void setName(String name);
+
+    /**
+     * Returns the size of the file in bytes.
+     *
+     * @return The size of the file as a long value in bytes.
+     */
+    long getSize();
+
+    /**
+     * Assigns the size of the file in bytes.
+     *
+     * @param size A long value representing the size of the file in bytes.
+     */
+    void setSize(long size);
+
+    /**
+     * Returns the data contained in the file.
+     *
+     * @return The file's data as a byte array.
+     */
+    byte[] getData();
+
+    /**
+     * Assigns data to the file.
+     *
+     * @param data A byte array representing the data to be assigned to the file.
+     */
+    void setData(byte[] data);
+
+    /**
+     * Returns the type of the file.
+     *
+     * @return The type of the file as a String.
+     */
+    String getType();
+
+    /**
+     * Assigns a type to the file.
+     *
+     * @param type A String representing the type of the file.
+     */
+    void setType(String type);
 }
